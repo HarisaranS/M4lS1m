011cb4056aa0aab0333fdbc43cb7247a744ac0ae
HarisaranS <harisaran777s@gmail.com>
2025-10-14 22:43:01 +0530

    Updated Net.java

--- javaFx/Net.java
+++ javaFx/Net.java
@@ -1,1 +1,15 @@
 //WebSocket Connections 
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Net {
+    public static void sendLine(String host, int port, String line) {
+        try (Socket s = new Socket(host, port);
+             PrintWriter pw = new PrintWriter(s.getOutputStream(), true)) {
+            pw.println(line);
+        } catch (Exception e) {
+            System.out.println("[NET] failed to send to " + host + ":" + port + " : " + e.getMessage());
+        }
+    }
+}
